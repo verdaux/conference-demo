d58e26bb7200f330fa14f3806a99ba3638f69f64
verdaux <xachineon@gmail.com>
2020-04-11 21:04:46 +0530

    save

--- src/main/java/com/sampleproject/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/sampleproject/conferencedemo/ConferenceDemoApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceDemoApplication {
+public class ConferenceDemoApplication
+{
 
 	public static void main(String[] args)
 	{
